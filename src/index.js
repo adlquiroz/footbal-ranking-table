87a2e34eceaf5828e1abc2d3c029501254cbbcdc
Octavio Quiroz <octavio.lopez@kavak.com>
2023-05-23 22:28:03 -0600

    se crea directorio y archivo de comandos para separar complejidad al index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require('./config.json');
+const { calificados } = require("./commands/calificados")
 const axios = require('axios');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -7,20 +8,19 @@ client.once(Events.ClientReady, () => {
     console.log('El Bot Liga MX esta en línea.')
 });
 
-client.on('interactionCreate', interaction => {
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
     const { commandName } = interaction;
+
     if (commandName === 'calificados') {
-        axios.get('https://ligamx.net/ws/aHR0cDovL3NpaWRhZG1pbi5saWdhbXgubmV0L3dlYnNlcnZpY2VzL3BydGxfd2ViX2pzb25kYXRhLmFzaHg@aHNhaD18NzBlMzJlY2E4ZTRmOWY5ZGVmYjVkZmVjNWEzNzVhMjg3N2QwN2M0Y2RkZTM1OWQ3ZDE2ODhhNDdlMmUwZWZlNDg2NTEyZmU4OTdkMzlhZmFmZTRhYmEyNzM2ODk2ZDViOGVjY2ZkZDhhZmIzYmNmMTA3NDFlMjY2ZjFiYWQ2MTN8JnBzV2lkZ2V0PVBSVExfQ2xzZlNtbGRMZ2xsVE9QJm9iaklERGl2aXNpb249MSZvYmpJRFRlbXBvcmFkYT03MyZvYmpJRFRvcm5lbz0yJm9ialRPUD0yOA==')
-            .then(async function (response) {
-                const tablaGeneral = response.data.DatosJSON;
-                const equiposCalificados = rankingleage(tablaGeneral, 12).join('\n');
-                await interaction.reply(equiposCalificados);
-            })
-            .catch(async (error) => {
-                console.log(error)
-                await interaction.reply('Error al obtener los datos');
-            })
+        try {
+            const equiposCalificados = await calificados();
+            await interaction.reply(equiposCalificados);
+
+        } catch (e) {
+            console.log(error)
+            await interaction.reply('Error al obtener los datos');
+        }
     } else if (commandName === 'tabla_general') {
         const team = interaction.options.getString('equipo');
         axios.get('https://ligamx.net/ws/aHR0cDovL3NpaWRhZG1pbi5saWdhbXgubmV0L3dlYnNlcnZpY2VzL3BydGxfd2ViX2pzb25kYXRhLmFzaHg@aHNhaD18NzBlMzJlY2E4ZTRmOWY5ZGVmYjVkZmVjNWEzNzVhMjg3N2QwN2M0Y2RkZTM1OWQ3ZDE2ODhhNDdlMmUwZWZlNDg2NTEyZmU4OTdkMzlhZmFmZTRhYmEyNzM2ODk2ZDViOGVjY2ZkZDhhZmIzYmNmMTA3NDFlMjY2ZjFiYWQ2MTN8JnBzV2lkZ2V0PVBSVExfQ2xzZlNtbGRMZ2xsVE9QJm9iaklERGl2aXNpb249MSZvYmpJRFRlbXBvcmFkYT03MyZvYmpJRFRvcm5lbz0yJm9ialRPUD0yOA==')
@@ -49,17 +49,6 @@ client.on('interactionCreate', interaction => {
 
 client.login(token);
 
-
-function rankingleage(tablaGeneral, topRank) {
-    const equiposCalificados = tablaGeneral.sort(
-        (elementoA, elementoB) => elementoA.Lugar - elementoB.Lugar
-    ).slice(0, topRank);
-    const nombreClubYRank = [];
-    for (let i = 0; i < equiposCalificados.length; i++) {
-        nombreClubYRank.push(`${equiposCalificados[i].Lugar}.- ${equiposCalificados[i].Club}`);
-    }
-    return nombreClubYRank;
-}
 
 function tablaLiga(tablaGeneral, topRank, team) {
     const equiposCalificados2 = tablaGeneral.sort(
